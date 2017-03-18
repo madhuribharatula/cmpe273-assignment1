f9ed28a0897161e44ddc404fc2fcb46fbd4bf54c
madhuri bharatula <madhuribharathula@gmail.com>
2017-03-17 20:22:27 -0700

    exception check added

--- app.py
+++ app.py
@@ -11,21 +11,28 @@ def helloWorld():
 @app.route('/v1/<fileName>')
 def git_read(fileName):
     print fileName
-    g = Github("madhuribharathula@gmail.com", "Gp2254131")
+    g = Github("madhuribharathula@gmail.com","Gp2254131")
     repoPaths = sys.argv[1].split("/")
     gitRepo = repoPaths[len(repoPaths) - 1]
     user = g.get_user()
     repo = user.get_repo(gitRepo)
     fileNameList = fileName.split(".")
     if fileNameList[len(fileNameList) -1] == "yml":
-        fileYml = repo.get_file_contents(fileName)
-        return yaml.dump(yaml.load(fileYml.decoded_content))
+        try:
+            fileYml = repo.get_file_contents(fileName)
+            return yaml.dump(yaml.load(fileYml.decoded_content))
+        except:
+            fileYml = repo.get_file_contents(fileNameList[0]+".json")
+            return yaml.dump(yaml.load(fileYml.decoded_content))
     elif fileNameList[len(fileNameList) -1] == "json":
-        fileJson = repo.get_file_contents(fileNameList[0]+".yml")
-        return json.dumps(yaml.load(fileJson.decoded_content),sort_keys=True, indent=2)
+        try:
+            fileJson = repo.get_file_contents(fileName)
+            return json.dumps(yaml.load(fileJson.decoded_content), sort_keys=True, indent=2)
+        except:
+            fileJson = repo.get_file_contents(fileNameList[0]+".yml")
+            return json.dumps(yaml.load(fileJson.decoded_content),sort_keys=True, indent=2)
     else:
         return "Must provide yml or json extension"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
-
